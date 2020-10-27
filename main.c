3a0c3965ef5f245e2efdaba29ca42d91b288d9b3
working <working@DESKTOP-17KGHMK>
2020-10-27 15:31:10 -0600

    - mi primer hola mundo

---
+++ main.c
@@ -0,0 +1,24 @@
+// encabezados
+#include <stdio.h>
+#include <stdlib.h>
+// definición y prototipos funciones
+
+// variables y constantes
+
+// implementación de funciones
+int main(int argc, char** argv) {
+
+    // declarar e inicar variables
+    
+    // variable (programación): espacio de memoria se manipula con un identidad (identifacador)
+    // declarando una variable 
+   
+    int x = 34;
+    int d = 56;
+    d = 456;
+         
+    printf("%d",x);
+    
+    return (0);
+}
+
